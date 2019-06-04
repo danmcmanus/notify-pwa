f5064411fba51e3f8e7d4d9d001098808e59f6be
danmcmanus <danmcmanus11@outlook.com>
2019-06-04 01:48:04 -0500

    initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,35 @@
+import { EventsService } from './../events.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { EventResponse } from '../interfaces';
+import { Subscription } from 'rxjs';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit, OnDestroy {
+  events: EventResponse[] = [];
+  sub: Subscription;
+
+  constructor(private eventsService: EventsService, private nav: NavController) {
+  }
+
+  ngOnInit(): void {
+    this.sub = this.eventsService.getAll()
+      .subscribe(e => this.events.push(e));
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  getEvents(): EventResponse[] {
+    return this.events.sort( (a, b) => a.event.created > b.event.created ? -1 : 1);
+  }
+
+  details(response: EventResponse) {
+    this.nav.navigateForward(`/details/${response.event.id}`);
+  }
+}
